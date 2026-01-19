05b0fc3526af0787764d594604b6d62e889e586b
The-Candyman <147785024+The-Candyman@users.noreply.github.com>
2026-01-19 06:05:57 +0000

    Constants adjusted
    Adjusted constants to match imports for TurretSystem commit.
    Also created new FieldConstants section for mostly field dimensions.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,13 +20,18 @@ public class Constants {
 
     public static class TurretConstants { // feet (NOT INCHES), seconds, degrees, pounds (mass), pound*ft/s^2 (force)
         public static final double k_gravitationalAcceleration = 32.174;
-        public static final double k_turretHeight = 2.0;
+        public static final double k_turretHeight = 2.0; // adjust to real design
+        public static final double k_extraTimeToPassSensor = 1.0; // test on field
+    }
+
+    public static class FieldConstants { // feet (NOT INCHES)
+        public static final double k_hubWidth = 41.7 / 12.0;
+        public static final double k_hubRadius = hubWidth / Math.sqrt(3.0);
         public static final double k_hubHeight = 6.0;
-        public static final double k_ceilingHeight = 15.0;
-        public static final double k_fuelRadius = 0.246063;
-        public static final double k_fuelMass = 0.474; // estimate
-        public static final double k_minYHeightToHub = 0.5 + k_fuelRadius + k_hubHeight - k_turretHeight; 
-        public static final double k_minYVelocityToHub = Math.sqrt(2.0 * k_gravitationalAcceleration * k_minYHeightToHub);
-        
-    } // TODO check and adjust constants
-}
+        public static final double k_hubX = 0.0; // adjust to field coordinate convention
+        public static final double k_hubY = (158.6 + (47.0 / 2)) / 12.0; // same as above
+        public static final double k_fuelRadius = 5.91 / 12.0 / 2.0;
+        public static final double k_fuelMass = 0.474; // average
+        public static final double k_ceilingHeight = 15.0; // estimated
+    }
+} // TODO check and adjust constants
