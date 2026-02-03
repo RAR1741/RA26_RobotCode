fa74062cd8e8cdf147ef3254c77046d54ac404b1
rar1741programmer <programmer@redalert1741.org>
2026-02-02 21:00:12 -0500

    just a little bit of asdfjklyuiefuie

--- src/main/java/frc/robot/subsystems/TurretSystem.java
+++ src/main/java/frc/robot/subsystems/TurretSystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -11,19 +12,9 @@ public class TurretSystem extends SubsystemBase {
 
     }
 
-    public static double calculateLaunchVelocity(double launchAngle, double xDisplacement, double yDisplacement) {
-        double cos = Math.cos(Math.toRadians(launchAngle));
-        double sin = Math.sin(Math.toRadians(launchAngle));
-        double divisor = 2.0 * cos * (xDisplacement * sin - yDisplacement * cos);
-        double radicand = Constants.TurretConstants.k_gravitationalAcceleration / divisor;
-        return xDisplacement * Math.sqrt(radicand);
-    }
-    public static double calculateLaunchAngle(double launchVelocity, double xDisplacement, double yDisplacement) {
-        return 0.0;
-    }
-    public static double calculateMaxHeightFromLaunch(double launchAngle, double launchVelocity) {
-        double vYSquared = Math.pow(launchVelocity * Math.sin(Math.toRadians(launchAngle)), 2.0);
-        double twoG = 2.0 * Constants.TurretConstants.k_gravitationalAcceleration;
-        return vYSquared / twoG;
+    public void periodic() {}
+
+    public Command aimToHub() {
+        return new Command();
     }
 }
