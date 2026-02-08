106ee0496bc792aa0bc147975178639dac9019a4
Patrick Frampton <patrick.frampton@gmail.com>
2026-02-08 13:15:13 -0500

    Clean up Simulation and register game pieces again

--- src/main/java/frc/robot/Simulation.java
+++ src/main/java/frc/robot/Simulation.java
@@ -1,40 +1,27 @@
 package frc.robot;
-import swervelib.simulation.ironmaple.simulation.SimulatedArena;
-import swervelib.simulation.ironmaple.simulation.drivesims.AbstractDriveTrainSimulation;
 
-import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import swervelib.simulation.ironmaple.simulation.SimulatedArena;
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.subsystems.SwerveSystem;
 
 public class Simulation {
     private SimulatedArena m_arena;
     private SwerveSystem m_swerve;
-    private SwerveDriveSimulation m_sim;
 
-    public Simulation(SwerveSystem swerveDrive){
+    public Simulation(SwerveSystem swerveDrive) {
         m_swerve = swerveDrive;
         m_arena = SimulatedArena.getInstance();
         m_arena.resetFieldForAuto();
-        Logger.recordOutput("FieldSimulation/Fuel", 
-            m_arena.getGamePiecesArrayByType("Fuel")
-        );
     }
 
-    public void init(){
-        m_sim = new SwerveDriveSimulation(
-                Constants.SimulationConstants.k_config,
-                new Pose2d(3, 3, new Rotation2d())
-            );
-
-//        SimulatedArena.getInstance().addDriveTrainSimulation(m_sim);
+    public void init() {
     }
 
-    public void periodic(){
+    public void periodic() {
         m_arena.simulationPeriodic();
+        Logger.recordOutput("FieldSimulation/Fuel",
+                m_arena.getGamePiecesArrayByType("Fuel"));
         Logger.recordOutput("Sim/RobotPose", m_swerve.getSwerveDrive().getPose());
-        System.out.println("" + m_swerve.getSwerveDrive().getPose());
     }
 }
