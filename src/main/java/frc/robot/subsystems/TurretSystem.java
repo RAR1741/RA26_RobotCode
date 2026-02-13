07ca8005d8bbde977e082c73e5fc13670913dbb1
Red Alert Programmer <programmer@redalert1741.org>
2026-02-12 20:51:11 -0500

    turretsystem is going from caterpillar to butterfly

--- src/main/java/frc/robot/subsystems/TurretSystem.java
+++ src/main/java/frc/robot/subsystems/TurretSystem.java
@@ -1,13 +1,59 @@
 package frc.robot.subsystems;
 
+import java.util.function.Supplier;
+
+// import org.littletonrobotics.junction.Logger;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
-// import frc.robot.ParabolicTrajectory;
+import frc.robot.Constants.TurretConstants;
+import frc.robot.ParabolicTrajectory;
 // import frc.robot.subsystems.LimelightSystem;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.local.SparkWrapper;
 
 public class TurretSystem extends SubsystemBase {
 
+    private final SparkMax leaderSpark = new SparkMax(TurretConstants.kLeaderMotorId,
+      MotorType.kBrushless);
+
+  private final SparkMax followerSpark = new SparkMax(TurretConstants.kFollowerMotorId,
+      MotorType.kBrushless);
+
+  private final SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+      .withFollowers(Pair.of(followerSpark, true))
+      .withControlMode(ControlMode.CLOSED_LOOP)
+      .withClosedLoopController(0.00936, 0, 0)
+      .withFeedforward(new SimpleMotorFeedforward(0.191, 0.11858, 0.0))
+      .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+      .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
+      .withMotorInverted(false)
+      .withIdleMode(MotorMode.COAST)
+      .withStatorCurrentLimit(Amps.of(40));
+
+  private final SmartMotorController smc = new SparkWrapper(leaderSpark, DCMotor.getNEO(2), smcConfig);
+
+  private final FlyWheelConfig shooterConfig = new FlyWheelConfig(smc)
+      .withDiameter(Inches.of(4))
+      .withMass(Pounds.of(1))
+      .withUpperSoftLimit(RPM.of(6000))
+      .withLowerSoftLimit(RPM.of(0))
+      .withTelemetry("Shooter", TelemetryVerbosity.HIGH);
+
+  private final FlyWheel shooter = new FlyWheel(shooterConfig);
 
     public TurretSystem() {
 
@@ -16,6 +62,6 @@ public class TurretSystem extends SubsystemBase {
     public void periodic() {}
 
     public Command aimToHub() {
-        return null;
+        return ;
     }
 }
