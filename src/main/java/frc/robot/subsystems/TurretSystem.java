058b2eb5f236f37ffbcc414b4e5ec1ce3621bf36
Red Alert Programmer <programmer@redalert1741.org>
2026-02-13 19:03:12 -0500

    copy paste go brrrr

--- src/main/java/frc/robot/subsystems/TurretSystem.java
+++ src/main/java/frc/robot/subsystems/TurretSystem.java
@@ -11,6 +11,20 @@ import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TurretConstants;
@@ -22,7 +36,9 @@ import yams.mechanisms.config.FlyWheelConfig;
 import yams.mechanisms.velocity.FlyWheel;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.local.SparkWrapper;
 
 public class TurretSystem extends SubsystemBase {
@@ -59,9 +75,84 @@ public class TurretSystem extends SubsystemBase {
 
     }
 
-    public void periodic() {}
+    public void periodic() {
+        Logger.recordOutput("Shooter/LeaderVelocity", leaderSpark.getEncoder().getVelocity());
+        Logger.recordOutput("Shooter/FollowerVelocity", followerSpark.getEncoder().getVelocity());
+    }
 
     public Command aimToHub() {
         return ;
     }
+
+    public Command setSpeed(AngularVelocity speed) {
+    return shooter.setSpeed(speed);
+  }
+
+  public Command setSpeedDynamic(Supplier<AngularVelocity> speedSupplier) {
+    return shooter.setSpeed(speedSupplier);
+  }
+
+  public Command spinUp() {
+    return setSpeed(RPM.of(5500));
+
+    // return setSpeed(RotationsPerSecond.of(50));
+
+    // return run(() -> {
+    // // followerNova.follow(leaderNova.getID());
+    // // followerNova.setInverted(true);
+
+    // // leaderNova.setPercent(SHOOTER_SPEED);
+    // // followerNova.setPercent(SHOOTER_SPEED);
+
+    // // followerNova.setPercent(0.5);
+    // });
+
+    // return shooter.set(0.5);
+    // return shooter.setSpeed(RotationsPerSecond.of(500));
+  }
+
+  public Command stop() {
+    return setSpeed(RPM.of(0));
+    // return run(() -> {
+
+    // // leaderNova.setPercent(0);
+    // // followerNova.setPercent(0);
+    // // followerNova.setPercent(0.5);
+    // });
+    // return shooter.set(0);
+  }
+
+  public AngularVelocity getSpeed() {
+    return shooter.getSpeed();
+  }
+
+  // public Command set(double dutyCycle) {
+  // return shooter.set(dutyCycle);
+  // }
+
+  public Command sysId() {
+    return shooter.sysId(Volts.of(12), Volts.of(3).per(Second), Seconds.of(7));
+  }
+
+  @Override
+  public void periodic() {
+    .recordOutput("Shooter/LeaderVelocity", leaderSpark.getEncoder().getVelocity());
+    Logger.recordOutput("Shooter/FollowerVelocity", followerSpark.getEncoder().getVelocity());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    shooter.simIterate();
+  }
+
+  private Distance wheelRadius() {
+    return Inches.of(4).div(2);
+  }
+
+  public LinearVelocity getTangentialVelocity() {
+    // Calculate tangential velocity at the edge of the wheel and convert to
+    // LinearVelocity
+
+    return MetersPerSecond.of(getSpeed().in(RadiansPerSecond)  * wheelRadius().in(Meters));
+  }
 }
