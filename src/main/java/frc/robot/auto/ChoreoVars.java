0045ad1f5f4c8b11a328a5ce733c3219a116176e
Jordan Miller <dracco1993@gmail.com>
2026-03-14 10:38:11 -0400

    More choreo testing

---
+++ src/main/java/frc/robot/auto/ChoreoVars.java
@@ -0,0 +1,20 @@
+// spotless:off
+package frc.robot.auto;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.*;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+
+    public static final class Poses {
+        public static final Pose2d Test = new Pose2d(2.3291936, 6.345108, Rotation2d.fromRadians(0));
+    }
+}
+// spotless:on
